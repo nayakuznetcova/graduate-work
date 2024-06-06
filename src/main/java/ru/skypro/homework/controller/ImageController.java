6a33ccbbf494700274742e4239541a4a0d65c3ff
Admin <samedi450511@gmail.com>
2024-06-06 22:36:05 +0600

    АВАТАР НАКОНЕЦ-ТО РАБОТАЕТ

--- src/main/java/ru/skypro/homework/controller/ImageController.java
+++ src/main/java/ru/skypro/homework/controller/ImageController.java
@@ -3,14 +3,11 @@ package ru.skypro.homework.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.skypro.homework.service.ImageService;
 
 import java.io.IOException;
-
+@CrossOrigin(value = "http://localhost:3000")
 @RestController
 @RequestMapping("/C:/avatar")
 @RequiredArgsConstructor
